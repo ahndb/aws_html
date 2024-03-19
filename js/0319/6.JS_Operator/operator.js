1966904c7d3f69340642c3fbe7670d91af45f097
ahndb <asi0000@nate.com>
2024-03-19 14:53:27 +0900

    240319

--- js/0319/6.JS_Operator/operator.js
+++ js/0319/6.JS_Operator/operator.js
@@ -13,9 +13,9 @@
   - / : 나눗셈
   - % : 나머지
 */
-console.log(10/3);
-console.log('십'/3);
-console.log(10/0);
+console.log(10 / 3);
+console.log("십" / 3);
+console.log(10 / 0);
 
 /*
   단항 산술 연산자 : 항이 하나인 산술 연산자
@@ -29,11 +29,11 @@ let single = 0;
 let result = 0;
 
 //* 선대입 후증가
-result = single++*8;
+result = single++ * 8;
 // console.log(result);  0
 
 //* 선증가 후대입
-result = ++single*8;
+result = ++single * 8;
 // console.log(result);  16
 
 //* 선대입 후감소
@@ -46,7 +46,7 @@ result = --single;
   - +단항 연산자는 숫자 타입이 아닌 피연산자에 사용하여 숫자 타입으로 변환함
 */
 console.log(typeof +10);
-console.log(typeof +'아10');
+console.log(typeof +"아10");
 console.log(+true);
 
 /*
@@ -54,18 +54,18 @@ console.log(+true);
   - 숫자 타입이 아닌 피연삱자에 사용하면 부호가 반전된 숫자 타입으로 변환함
 */
 console.log(-10);
-console.log(-'qweqeqwe10');
+console.log(-"qweqeqwe10");
 console.log(-true);
 
 /*
   문자열 연결 연산자
   - + 이항 연산자의 피연산자 중 하나라도 문자열이 존재하면 연결 연산자로 사용됨
 */
-console.log(typeof ('1' + 1));
+console.log(typeof ("1" + 1));
 console.log(+false);
 console.log(-false);
-console.log('a' + (10+20)+'a');
-console.log(typeof (123-'asd'-'123'));
+console.log("a" + (10 + 20) + "a");
+console.log(typeof (123 - "asd" - "123"));
 /*
   할당 연산자 : 우항에 있는 피연산자와 좌항을 연산하여 좌항에 대입
   -  = : 좌항에 우항을 대입
@@ -83,9 +83,8 @@ console.log(typeof (123-'asd'-'123'));
   - != : 부등 비교
   - !== 불일치 비교
 */
-console.log(10 == '10');
-console.log(10 === '10');
-
+console.log(10 == "10");
+console.log(10 === "10");
 
 /*
   대소 관계 비교 연산자 : 피연산자의 크기를 비교하여 논리 값으로 반환
@@ -94,8 +93,7 @@ console.log(10 === '10');
   - >= : 좌항이 우항보다 크거나 같다
   - <= : 좌항이 우항보다 작거나 같다
 */
-console.log(10 > '9');
-
+console.log(10 > "9");
 
 /*
   삼항 연산자 : 조건식의 논리 결과에 따라 서로 다른 결과를 반환
@@ -103,28 +101,28 @@ console.log(10 > '9');
 */
 const number = 9;
 // const result2 = number % 2 === 0 ? '짝수' : '홀수';
-let result2 = number % 2 ? '홀수' : '짝수';
+let result2 = number % 2 ? "홀수" : "짝수";
 console.log(result2);
 
 if (number % 2) {
-  result2 = '홀수';
+  result2 = "홀수";
 } else {
-  result2 = '짝수';
+  result2 = "짝수";
 }
 
 /*
   삼항 연산자는 if else 문과 같은 역할을 함
 */
-let result3 = '';
+let result3 = "";
 if (number > 0) {
-  result3 = '양수';
+  result3 = "양수";
 } else if (number < 0) {
-  result3 = '음수';
+  result3 = "음수";
 } else {
-  result3 = '영';
+  result3 = "영";
 }
 
-result3 = number < 0 ? '음수' : number > 0 ? '양수' : '영';
+result3 = number < 0 ? "음수" : number > 0 ? "양수" : "영";
 console.log(result3);
 
 /*
@@ -133,13 +131,12 @@ console.log(result3);
   - && : 좌항과 우항이 모두 true 이면 true, 그렇지 않으면 false
   - ! : 우항을 부정, 우항이 true이면 false, false이면 true
 */
-console.log(true||'홍길동');
-console.log(false ||'홍길동');
-console.log(true && '홍길동');
-console.log(false && '홍길동');
+console.log(true || "홍길동");
+console.log(false || "홍길동");
+console.log(true && "홍길동");
+console.log(false && "홍길동");
 
-console.log('홍길동'&&true);
-
+console.log("홍길동" && true);
 
 /*
   - ||연산자와 &&연산자의 결과는 논리 값이 아닐 수 있음
@@ -148,9 +145,9 @@ console.log('홍길동'&&true);
 /*
   쉼표 연산자 : 왼쪽 피연산자부터 차례로 피연산자를 연산
 */
-let x,y,z;
+let x, y, z;
 console.log(x);
-x = 10, y = 20, z = 30;
+(x = 10), (y = 20), (z = 30);
 console.log(x);
 /*
   typeof 연산자 : 우항의 데이터 및 변수의 타입을 문자열로 반환
