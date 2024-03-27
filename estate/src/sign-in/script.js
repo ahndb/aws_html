3df7bedbeff7b1c7bbee88d8ca7ff081af6b0ee5
ahndb <asi0000@nate.com>
2024-03-27 09:50:26 +0900

    09:50 Create common.js And Update signUp.js

--- estate/src/sign-in/script.js
+++ estate/src/sign-in/script.js
@@ -1,33 +1,23 @@
-const ID = "userIdentity";
-const PASSWORD = "P!ssw0rd";
+const ID = 'userIdentity';
+const PASSWORD = 'P!ssw0rd';
 
 function onSignInButtonClickHandler(event) {
-  const id = document.getElementById("id").value;
-  const password = document.getElementById("password").value;
+    const id = document.getElementById('id').value;
+    const password = document.getElementById('password').value;
 
-  const signInMessageElement = document.getElementById("sign-in-message");
-  if (id === ID && password === PASSWORD) {
-    alert("로그인 성공!");
-    signInMessageElement.textContent = "";
-  } else {
-    
-    signInMessageElement.textContent = "로그인 정보가 일치하지 않습니다.";
-  }
+    const signInMessageElement = document.getElementById('sign-in-message');
+    if (id === ID && password === PASSWORD) {
+        alert('로그인 성공!');
+        signInMessageElement.textContent = '';
+    } else {
+        signInMessageElement.textContent = '로그인 정보가 일치하지 않습니다.';
+    }
 }
+
 function onSignUpLinkClickHandler(event) {
-  window.location.href = "https://google.com";
+    window.location.href = '../sign-up';
 }
 
-const signUpLinkElement = document.getElementById("sign-up-link");
-signUpLinkElement.addEventListener("click", onSignUpLinkClickHandler);
+const signUpLinkElement = document.getElementById('sign-up-link');
+signUpLinkElement.addEventListener('click', onSignUpLinkClickHandler);
 
-function kakao(event) {
-  window.location.href = "https://developers.kakao.com";
-}
-
-function naver(event) {
-  window.location.href = "https://naver.com";
-}
-
-const snsLoginNaver = document.getElementById("naver");
-snsLoginNaver.addEventListener("click", naver);
