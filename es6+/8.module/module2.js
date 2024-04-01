c97f101c51c428f22df28a84338685c9a55a2a3b
ahndb <asi0000@nate.com>
2024-04-01 10:52:45 +0900

    240401 10:50 Update_module

--- es6+/8.module/module2.js
+++ es6+/8.module/module2.js
@@ -1,28 +1,21 @@
 /*
-    import from:
-    - 
-    - 
-    - 
+  import from:
+  - 타 모듈에서 export 한 대상을 로드 
+  - 타 모듈에서 export 내보내면 { }를 사용하여 import 해야함, 이름이 같아야함
+  - 
 */
-
-
-
+import { exportVariable, exprotFunction, exportClass } from './module.mjs'
+console.log(exportVariable);
 
 /*
-    - 
+  - 
 */
-
-
 
 /*
-    - 
-    - 
+  - 
+  - 
 */
-
-
 
 /*
-    - 
+  - 
 */
-
-
